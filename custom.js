1f39fe9d9e1b464f363b9e12be04f1a8f3487347
KarthikKumarMamidi <karthik.m@ibhubs.co>
2020-03-06 13:24:56 +0530

    modified custom js

--- custom.js
+++ custom.js
@@ -4,12 +4,15 @@ Jupyter.toolbar.add_buttons_group([
          'icon'    : 'fa-terminal',
          'callback': function(){
          	var name = prompt("Enter user name to submit");
+         	user_data = {
+         		"name": name
+         	}
          	$.ajax({
 				type: "POST",
 				contentType: 'application/json',
             	dataType: 'json',
-				url: "~/api/test.py",
-				data: { "name": name },
+				url: "/api/test.py",
+				data: JSON.stringify(user_data)
 				success: function(response){
 					console.log(response);
 				}
