8d6981ec1c40ac1a7ee1460cf809eba23c4f09ea
KarthikKumarMamidi <karthik.m@ibhubs.co>
2020-03-06 12:10:52 +0530

    added custom js

--- custom.js
+++ custom.js
@@ -1,32 +1,30 @@
-define([
-    'jquery',
-    'base/js/namespace',
-    'base/js/dialog',
-    'base/js/events',
-    'base/js/utils',
-    'require',
-], function(
-    $,
-    Jupyter,
-    dialog,
-    events,
-    utils,
-    require
-) {
-    "use strict";
+var label = jQuery('<label/>').text('AutoScroll Limit:');
+var select = jQuery('<select/>')
+     //.append(jQuery('<option/>').attr('value', '2').text('2min (default)'))
+     .append(jQuery('<option/>').attr('value', undefined).text('disabled'))
 
-    var load_ipython_extension = function() {
+     // TODO:
+     //the_toolbar_element.append(label)
+     //the_toolbar_element.append(select);
 
-        // add button to toolbar in case appmode is not enabled
-        Jupyter.toolbar.add_buttons_group([{
-            id : 'toggle_codecells',
-            label : 'Appmode',
-            icon : 'fa-arrows-alt',
-            callback : goto_app_mode
-        }]);
-    };
+select.change(function() {
+     var val = jQuery(this).val() // val will be the value in [2]
+     // TODO
+     // this will be called when dropdown changes
 
-    return {
-        load_ipython_extension : load_ipython_extension
-    };
 });
+
+var time_m = [1,5,10,15,30];
+for (var i=0; i < time_m.length; i++) {
+     var ts = time_m[i];
+                                          //[2]   ____ this will be `val` on [1]
+                                          //     |
+                                          //     v
+     select.append($('<option/>').attr('value', ts).text(thr+'min'));
+     // this will fill up the dropdown `select` with
+     // 1 min
+     // 5 min
+     // 10 min
+     // 10 min
+     // ...
+}
