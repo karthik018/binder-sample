b76ab629783f1f8f5ebd3241eea6e804109cdea3
KarthikKumarMamidi <karthik.m@ibhubs.co>
2020-03-06 12:23:11 +0530

    modified custom js

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
 Jupyter.toolbar.add_buttons_group([
     {
-         'label'   : 'run qtconsole',
+         'label'   : 'Submit',
          'icon'    : 'fa-terminal', // select your icon from
                                       // http://fontawesome.io/icons/
          'callback': function(){alert("Hi How are you?")}
