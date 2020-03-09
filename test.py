23e875192e256061de452c2b58b9534595c125e4
KarthikKumarMamidi <karthik.m@ibhubs.co>
2020-03-09 11:00:00 +0530

    changed python response

--- test.py
+++ test.py
@@ -6,4 +6,3 @@ def test_method(name):
 	response = requests.get(url="http://demo7636223.mockable.io/submit")
 	response = response.json()
 	click.echo("response: "+response["msg"])
-	click.echo("Hello "+name)
