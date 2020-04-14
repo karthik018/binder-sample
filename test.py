99d32aafc34fea7ec6404a4c6e283e3c5a680f81
KarthikKumarMamidi <karthik.m@ibhubs.co>
2020-04-14 13:05:02 +0530

    changed test py

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ import requests
 
 class Utils:
 
-	def test_method(name):
+	def test_method(self, name):
 		response = requests.get(url="http://demo7636223.mockable.io/submit")
 		response = response.json()
 		click.echo("response: "+response["msg"])
 		
 		
-	def get_user_nb(name):
+	def get_user_nb(self, name):
 		response = requests.get(url="https://ib-mattermost.s3.ap-south-1.amazonaws.com/notebooks/MyJupyter.ipynb")
-		open("../default/sample.ipynb", 'wb').write(response.content)
+		open("../sample.ipynb", 'wb').write(response.content)
 		click.echo("response: "+str(response.status_code))
 	
