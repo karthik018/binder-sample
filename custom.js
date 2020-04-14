f1f16025fbf88409ab27f40787e334f72c193409
KarthikKumarMamidi <karthik.m@ibhubs.co>
2020-04-14 14:22:40 +0530

    added button to get user_nb

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 const submit = (name) =>
     new Promise(resolve => {
         const jvnLog = data => {
-          console.log(data)
+          console.log(data);
           resolve(data.content.text.trim());
         };
         const run_submit = "from api.test import Utils\n" + "import io\n" +
@@ -18,6 +18,26 @@ const submit = (name) =>
         });
     });
     
+const get_user_nb = (name) =>
+	new Promise(resolve => {
+		const jvnLog = data => {
+			console.log(data);
+			resolve(data.content.text.trim());
+		};
+		const run_get_user_nb = "from api.test import Utils\n" + "import io\n" +
+			"from contextlib import redirect_stdout\n" +
+			"f = io.StringIO()\n" + 
+			"with redirect_stdout(f):\n" +
+			"\t" + "Utils().get_user_nb(" + '"' + name + '"' + ")\n" +
+			"out = f.getvalue().splitlines()[-1]\n" + "print(out)";
+		Jupyter.notebook.save_checkpoint();
+		Jupyter.notebook.events.one("notebook_saved.Notebook", function() {
+			Jupyter.notebook.kernel.execute(run_get_user_nb, {
+				iopub: { output: jvnLog }
+			});
+		});
+	});
+    
 Jupyter.toolbar.add_buttons_group([
     {
          'label'   : 'Submit',
@@ -28,5 +48,15 @@ Jupyter.toolbar.add_buttons_group([
          	    alert(log_data)
          	});
          }
+    },
+    {
+    	'label'    : 'User-NB',
+    	'icon'     : 'fa-cloud-download fa',
+    	'callback' : function(){
+    		var name = prompt("Enter user name to get user notebook");
+    		get_user_nb(name).then(log_data => {
+    			alert(log_data)
+    		});
+    	}
     }
 ]);
