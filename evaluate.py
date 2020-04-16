a8e1d1c70b39afb075461a6f7bb6ba4e4cd09348
KarthikKumarMamidi <karthik.m@ibhubs.co>
2020-04-16 12:21:03 +0530

    added new files to check final flow

---
+++ evaluate.py
@@ -0,0 +1,10 @@
+class Evaluate:
+	def get_evaluation_metrics(self):
+		response = {
+			"metrics": {
+				"key": "MSE",
+				"value": 100
+			}
+		}
+		
+		return response
--- test.py
+++ test.py
@@ -1,49 +1,32 @@
 import click
 import requests
+from evaluate import Evaluate
 
 
 class Utils:
-
+	
+	def get_metadata(data):
+		url = "http://demo8180972.mockable.io/metadata"
+		response = requests.post(url=url, data=data)
+		response = response.json()
+		return response["metadata"]
+	
 	def test_method(self, name):
-		cell_num = 1
-		with open("./sample.ipynb", "r") as file:
-			content = file.read()
-			import json
-			content = json.loads(content)
-			print(content)
-			code_list = content['cells'][cell_num]['source']
-			code = ''.join([line for line in code_list])
-			from io import StringIO
-			import sys
-			codeOut = StringIO()
-			codeErr = StringIO()
-			sys.stdout = codeOut
-			sys.stderr = codeErr
-			exec(code)
-			n_numbers(200)
-			sys.stdout = sys.__stdout__
-			sys.stderr = sys.__stderr__
-			s = codeOut.getvalue()
-			output = list(s.split('\n'))
-			text = [each + '\n' for each in output]
-			outputs = [
-				{
-					"name": "stdout",
-					"output_type": "stream",
-					"text": text
-				}
-			]
-			content["cells"][cell_num]["outputs"] = outputs
-			json.dump(content, file, ensure_ascii=False, indent=1)
-			file.close()
-			
-		response = requests.get(url="http://demo7636223.mockable.io/submit")
+		metadata = self.get_metadata(name)
+		
+		metrics = Evaluate().get_evaluation_metrics()
+		response = requests.post(url="http://demo7636223.mockable.io/submit", data=metrics)
 		response = response.json()
 		click.echo("response: "+response["msg"])
 		
 		
 	def get_user_nb(self, name):
-		response = requests.get(url="https://ib-mattermost.s3.ap-south-1.amazonaws.com/notebooks/MyJupyter.ipynb")
+		url = "http://demo1984069.mockable.io/latest/notebook"
+		response = requests.post(url=url, data=name)
+		response = response.json()
+		notebook_url = response["notebook_url"]
+		
+		response = requests.get(url=notebook_url)
 		open("./sample.ipynb", 'wb').write(response.content)
 		click.echo("Got Notebook Successfully with status: "+str(response.status_code))			
 	
